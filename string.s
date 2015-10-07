85c33c5d7aa056ece8fa98b6dccf293cf66f14e9
Gloria Miao <miaog@berkeley.edu>
2015-10-07 06:26:29 +0000

    finished part1

---
+++ string.s
@@ -0,0 +1,170 @@
+# CS 61C Summer 2015 Project 2-2 
+# string.s
+
+#==============================================================================
+#                              Project 2-2 Part 1
+#                               String README
+#==============================================================================
+# In this file you will be implementing some utilities for manipulating strings.
+# The functions you need to implement are:
+#  - strlen()
+#  - strncpy()
+#  - copy_of_str()
+# Test cases are in linker-tests/test_string.s
+#==============================================================================
+
+.data
+newline:	.asciiz "\n"
+tab:	.asciiz "\t"
+
+.text
+#------------------------------------------------------------------------------
+# function strlen()
+#------------------------------------------------------------------------------
+# Arguments:
+#  $a0 = string input
+#
+# Returns: the length of the string
+#------------------------------------------------------------------------------
+strlen:
+	addiu $t0, $0, 0 #counter
+
+str_loop:
+	lb $t1, 0($a0)
+	beq $t1, $0, str_exit
+	addiu $t0, $t0, 1 #increment counter
+	addiu $a0, $a0, 1
+	j str_loop
+
+str_exit:
+	addiu $v0, $t0, 0
+	jr $ra
+
+#------------------------------------------------------------------------------
+# function strncpy()
+#------------------------------------------------------------------------------
+# Arguments:
+#  $a0 = pointer to destination array
+#  $a1 = source string
+#  $a2 = number of characters to copy
+#
+# Returns: the destination array
+#------------------------------------------------------------------------------
+strncpy:
+	addiu $t0, $0, 0 #counter 
+
+strn_loop:
+	add $t1, $t0, $a1 #t1 = a1
+	beq $a2, 0, strn_exit 
+	lb $t2, 0($t1) #t2 = a1[i]
+	addu $t3, $t0, $a0 #t3 = a0[i]
+	sb $t2, 0($t3)
+	subu $a2, $a2, 1
+	addiu $t0, $t0, 1
+	beq $t1, 0, strn_exit
+	j strn_loop
+
+strn_exit:
+	addiu $v0, $a0, 0
+	jr $ra
+
+#------------------------------------------------------------------------------
+# function copy_of_str()
+#------------------------------------------------------------------------------
+# Creates a copy of a string. You will need to use sbrk (syscall 9) to allocate
+# space for the string. strlen() and strncpy() will be helpful for this function.
+# In MARS, to malloc memory use the sbrk syscall (syscall 9). See help for details.
+#
+# Arguments:
+#   $a0 = string to copy
+#
+# Returns: pointer to the copy of the string
+#------------------------------------------------------------------------------
+copy_of_str:
+	addi $sp, $sp, -16
+	sw $ra, 0($sp)
+	sw $a0, 4($sp)
+	sw $s0, 8($sp)
+	sw $s1, 12($sp)
+	addiu $s0, $a0, 0
+	jal strlen
+	addiu $s1, $v0, 0 #s1 = strlen
+	addiu $a0, $v0, 1
+	addiu $v0, $0, 9 #malloc
+	syscall
+	addiu $a0, $v0, 0
+	addiu $a1, $s0, 0
+	addiu $a2, $s1, 0
+	jal strncpy
+	lw $ra, 0($sp)
+	lw $a0, 4($sp)
+	lw $s0, 8($sp)
+	lw $s1, 12($sp)
+	addiu $sp, $sp, 16
+	jr $ra
+
+###############################################################################
+#                 DO NOT MODIFY ANYTHING BELOW THIS POINT                       
+###############################################################################
+
+#------------------------------------------------------------------------------
+# function streq() - DO NOT MODIFY THIS FUNCTION
+#------------------------------------------------------------------------------
+# Arguments:
+#  $a0 = string 1
+#  $a1 = string 2
+#
+# Returns: 0 if string 1 and string 2 are equal, -1 if they are not equal
+#------------------------------------------------------------------------------
+streq:
+	beq $a0, $0, streq_false	# Begin streq()
+	beq $a1, $0, streq_false
+streq_loop:
+	lb $t0, 0($a0)
+	lb $t1, 0($a1)
+	addiu $a0, $a0, 1
+	addiu $a1, $a1, 1
+	bne $t0, $t1, streq_false
+	beq $t0, $0, streq_true
+	j streq_loop
+streq_true:
+	li $v0, 0
+	jr $ra
+streq_false:
+	li $v0, -1
+	jr $ra			# End streq()
+
+#------------------------------------------------------------------------------
+# function dec_to_str() - DO NOT MODIFY THIS FUNCTION
+#------------------------------------------------------------------------------
+# Convert a number to its unsigned decimal integer string representation, eg.
+# 35 => "35", 1024 => "1024". 
+#
+# Arguments:
+#  $a0 = int to write
+#  $a1 = character buffer to write into
+#
+# Returns: the number of digits written
+#------------------------------------------------------------------------------
+dec_to_str:
+	li $t0, 10			# Begin dec_to_str()
+	li $v0, 0
+dec_to_str_largest_divisor:
+	div $a0, $t0
+	mflo $t1		# Quotient
+	beq $t1, $0, dec_to_str_next
+	mul $t0, $t0, 10
+	j dec_to_str_largest_divisor
+dec_to_str_next:
+	mfhi $t2		# Remainder
+dec_to_str_write:
+	div $t0, $t0, 10	# Largest divisible amount
+	div $t2, $t0
+	mflo $t3		# extract digit to write
+	addiu $t3, $t3, 48	# convert num -> ASCII
+	sb $t3, 0($a1)
+	addiu $a1, $a1, 1
+	addiu $v0, $v0, 1
+	mfhi $t2		# setup for next round
+	bne $t2, $0, dec_to_str_write
+	jr $ra			# End dec_to_str()
